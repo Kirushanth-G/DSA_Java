6643adad599173dad93571c8342ba3b051f8be4d
Kiru <kirushanthh13@gmail.com>
2025-05-22 16:50:30 +0530

    Basic implementation

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue queue1 = new Queue(6);
+        queue1.enqueue(1);
+        queue1.enqueue(2);
+        queue1.enqueue(3);
+    }
+}
--- src/Queue.java
+++ src/Queue.java
@@ -1,2 +1,22 @@
 public class Queue {
+    private int[] arr;
+    private int capacity;
+    private int end = 0;
+
+    public Queue(int capacity) {
+        this.arr = new int[capacity];
+        this.capacity = capacity;
+    }
+
+    public void enqueue(int num_to_insert){
+        arr[end + 1] = num_to_insert;
+        System.out.println("number: "+num_to_insert+" inserted successfully");
+    }
+
+    public int dequeue(){
+        int num_removed = arr[0];
+        return num_removed;
+    }
+
+
 }
